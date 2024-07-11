7a2752f435b831c2548760d18ba0f140672d6f09
uflorez <urielflorez29@gmail.com>
2024-07-11 16:48:07 -0500

    Proyecto login

---
+++ login_app/lib/mocks/mocks_constants.dart
@@ -0,0 +1,7 @@
+const mockPassword = '123456a.'; // Simulated secure password
+const mockToken = 'mockToken123456'; // Simulated token
+const listaUsuarios = [
+  'usuario1',
+  'usuario2',
+  'usuario3',
+];
